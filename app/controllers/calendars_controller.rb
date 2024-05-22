6adf973be88ade54b2af93eccd649b21429fe1c7
kikusachi <168533677+kikusachi@users.noreply.github.com>
2024-05-22 17:05:49 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -37,7 +37,16 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+
+      # wdayメソッドを用いて取得した数値
+      wday_num = (@todays_date + x).wday
+      #「wday_numが7以上の場合」という条件式
+      if wday_num >= 7
+        wday_num = wday_num -7
+      end
+
+      #wdaysを使うと曜日が取り出せる・(@todays_date + x)で日にちをずらし目的の日数分取得
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wdays => wdays[wday_num]}
       @week_days.push(days)
     end
 
